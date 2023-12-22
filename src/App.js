8ac68a2b74829b4fe006f8dc291d7899446a91eb
James Keary <jkeary@gmail.com>
2023-12-22 16:15:24 -0500

    event triggers and listener proof of concept

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Calculator from "awesome-react-calculator";
 
 const App = () => {
   const handleInput = (input) => {
-    console.log(
-      `${input.expression} is shown in the calculator, User clicked the ${input.key}`
-    );
+    // console.log(
+    //   `${input.expression} is shown in the calculator, User clicked the ${input.key}`
+    // );
   };
 
   const onResultChange = (newResult) => {
-    console.log(newResult);
-    console.log(`${newResult.expression} is validated as ${newResult.result} `);
+    // console.log(newResult);
+    // console.log(`${newResult.expression} is validated as ${newResult.result} `);
   };
 
   return (
-    <div style={{ height: "400px", width: "100%", fontFamily: "Open Sans" }}>
-      <Calculator onNewInput={handleInput} onResultChange={onResultChange} />
+    <div style={{ height: "400px", width: "100%" }}>
+      <Calculator onNewInput={handleInput} onResultChange={onResultChange}/>
     </div>
   );
 };
